e1bc9d39fbeb24c12abcc989b4bb3584c3fa674c
Robinson Labourdette <robinson@inoft.com>
2021-10-11 18:28:38 +0200

    Copied unittest of SectionedObjectStore to BasicObjectStore

---
+++ tests/BasicObjectStore.test.old.ts
@@ -0,0 +1,24 @@
+import {BasicObjectStore} from "../src/Stores/ObjectStores/BasicObjectStore";
+import {BaseFieldModel, MapModel} from "../src/ModelsFields";
+
+interface StoreObjectModel {
+    container1: {
+        field1: number;
+    }
+}
+
+describe('BasicObjectStore', () => {
+    test('Simple attribute retrieval', async () => {
+        const store = new BasicObjectStore<StoreObjectModel>({
+            retrieveDataCallable: () => Promise.resolve(undefined),
+            objectModel: new MapModel({fields: {
+                'container1': new MapModel({fields: {
+                    'field1': new BaseFieldModel({})
+                }})
+            }})}
+        );
+        store.loadFromData({'container1': {'field1': 42}});
+        const retrievedField1Value: number | undefined = await store.getAttr('container1.field1');
+        expect(retrievedField1Value).toEqual(42);
+    });
+});
