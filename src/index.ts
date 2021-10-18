b61abcc82b29fc738d1344014c9ea31db8e2869d
Robinson Labourdette <robinson@inoft.com>
2021-10-18 14:50:10 +0200

    v2.1.4

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import 'Stores/BaseStore';
+import 'Stores/ValueStore';
+import 'Stores/ObjectStores/BaseObjectStore';
+import 'Stores/ObjectStores/BasicObjectStore';
+import 'Stores/ObjectStores/ItemsObjectStores/BaseItemsObjectStore';
+import 'Stores/ObjectStores/ItemsObjectStores/BasicItemsObjectStore';
+import 'Stores/ObjectStores/ItemsObjectStores/SectionedItemsObjectStore';
