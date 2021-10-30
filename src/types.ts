9d6878adb4f8538cc3c2dcc00a93bf63904c07ed
Robinson Labourdette <robinson@inoft.com>
2021-10-30 17:48:27 +0200

    v3.0.9 - Fixed bug with CastListToImmutable of ImmutableCast

--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,11 @@ import {U, L, O, S, A} from "ts-toolbelt";
 import * as immutable from "immutable";
 
 
-/** Cast all object items to the ImmutableCast **/
+/** Cast all object items to the ImmutableCast and wrap them in an immutable.RecordOf **/
 export type CastObjectToImmutable<T extends object> = immutable.RecordOf<U.Merge<{ [K in A.Keys<T>]: ImmutableCast<A.At<T, K>> }>>;
 
-/** Cast all array items to the ImmutableCast **/
-export type CastListToImmutable<T extends any[]> = immutable.List<O.UnionOf<{ [K in A.Keys<T>]: ImmutableCast<A.At<T, K>> }>>;
- // todo: simplify CastListToImmutable
+/** Cast all array items to the ImmutableCast and wrap them in an immutable.List **/
+export type CastListToImmutable<T extends any[]> = immutable.List<ImmutableCast<T[any]>>;
 
 /**
  * Recursively cast all array's to immutable.List's and objects to immutable.Record's
